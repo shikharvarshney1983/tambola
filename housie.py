533c9b2703fc071857baa6e7dff6a26e6340bdc3
shikharvarshney1983 <86799396+shikharvarshney1983@users.noreply.github.com>
2021-07-02 23:11:43 +0530

    Add files via upload

--- housie.py
+++ housie.py
@@ -1,27 +1,41 @@
 import random
 from flask import Flask, request, session, render_template 
+from ticket import *
 
 app = Flask(__name__)
 app.secret_key = b"\xdb\xd8\xcb)@\xbbi\xcc\xd4\xb8\xbcz+5]g\xb6\x1d\xf3\xf4'\xf7\x82["
+
+ticket = getLines(retTicket())
+print(ticket)
 
 @app.route('/')
 def index():
     session['numbers'] = random.sample(range(1,91),90)
     session['count'] = 0
-    return render_template('index.html', nums=['0'])
+    session['ticket'] = ticket
+    return render_template('index.html', nums=['0'], ticket = session['ticket'])
 
 @app.route('/startnew')
 def startnew():
-    global numbers, count
+    global numbers, count, ticket
     session['count'] = 0
     session['numbers'] = random.sample(range(1,91),90)
-    return render_template('index.html', nums=['0'])
+    session['ticket'] = ticket
+    print(session['ticket'])
+    return render_template('index.html', nums=['0'], ticket = session['ticket'])
 
 @app.route('/next', methods=['POST'])
 def next():
-    global count
+    global count, numbers, ticket
     session['count'] = session['count'] + 1
-    return render_template('index.html', nums=session['numbers'][:session['count']])
+    return render_template('index.html', nums=session['numbers'][:session['count']], ticket = session['ticket'])
+
+@app.route('/generateTicket', methods=['POST','GET'])
+def generateTicket():
+    global count, numbers, ticket
+    session['ticket'] = getLines(retTicket())
+    return render_template('index.html', nums=session['numbers'][:session['count']], ticket = session['ticket'])
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=81)
--- ticket.py
+++ ticket.py
@@ -38,34 +38,73 @@ def retTicket():
     numbers.sort()
     return numbers
 
-def chkLstCount():
-    pass
+def chkLstCount(lstNum):
+    lst=[0,0,0,0,0,0,0,0,0]
+    retLst=[[],[],[],[],[],[],[],[],[]]
+    for nm in lstNum:
+        for i in range(0,71,10):
+            if nm >= i and nm < i+10:
+                lst[int(i/10)] = lst[int(i/10)] + 1
+                retLst[int(i/10)].append(nm)
+        if nm >= 80 and nm < 91:
+            lst[8] = lst[8] + 1
+            retLst[8].append(nm)
+    return lst, retLst
 
-def getSingleNumLst(lstNum, linenum):
+def getSingleNumLst(lstNum):
     ret = []
     lstNum.sort()
     for i in lstNum:
         if checkValid(ret, i, 1):
             ret.append(i)
     return ret   
+
+def createLinewithBlank(line):
+    lst = ['','','','','','','','','']
+    for i in line:
+        index = int(i/10) if i != 90 else 8
+        lst[index] = str(i)
+    return lst
     
 def getLines(lstNum):
-    ret = []
-    for i in range (0,3):
-        lst = []
-        tmpLstNum = getSingleNumLst(lstNum)
-        print("tmpLstNum = ",tmpLstNum)
-        for j in range (0,5):
-            nm = random.sample(tmpLstNum,1)[0]
-            lst.append(nm)
-            lstNum.remove(nm)
-            tmpLstNum.remove(nm)
-        lst.sort()
-        print("lst = ",lst)
-        ret.append(lst)
-    return ret
-            
-numbers = retTicket()
-print("numbers =" ,numbers)
-lines = getLines(numbers)
-print(lines)
+    line1 = []
+    line2 = []
+    line3 = []
+    val, lst = chkLstCount(lstNum)
+    for i in range(0,9):
+        if val[i] == 3:
+            line1.append(lst[i][0])
+            lstNum.remove(lst[i][0])
+            line2.append(lst[i][1])
+            lstNum.remove(lst[i][1])
+            line3.append(lst[i][2])
+            lstNum.remove(lst[i][2])
+    tmpLstNum = getSingleNumLst(lstNum)
+    for i in range(5 - len(line1)):
+        nm = random.sample(tmpLstNum,1)[0]
+        line1.append(nm)
+        lstNum.remove(nm)
+        tmpLstNum.remove(nm)
+    val, lst = chkLstCount(lstNum)
+    for i in range(len(val)):
+        if val[i] == 2:
+            line2.append(lst[i][0])
+            lstNum.remove(lst[i][0])
+            line3.append(lst[i][1])
+            lstNum.remove(lst[i][1])
+    for i in range(5-len(line2)):
+        nm = random.sample(lstNum,1)[0]
+        line2.append(nm)
+        lstNum.remove(nm)
+    for nm in lstNum:
+        line3.append(nm)
+    line1 = createLinewithBlank(line1)
+    line2 = createLinewithBlank(line2)
+    line3 = createLinewithBlank(line3)
+    return [line1,line2,line3]
+
+if __name__ == "__main__":
+    numbers = retTicket()
+    print("numbers =" ,numbers)
+    lines = getLines(numbers)
+    print(lines)
